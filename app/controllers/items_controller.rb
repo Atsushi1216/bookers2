d7efb2ac26d7a983555682f8648c2ff347509960
Atsushi1216 <ats1216@gmail.com>
2021-12-25 05:09:20 +0000

    [Add]itemコントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,22 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @item = Item.new
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+     @item = Item.find(params[:id])
   end
 
   def update
+    if @item.update(book_params)
+      redirect_to item_path(@item.id), notice: 'successfully'
+    else
+      render action: :edit
+    end
   end
 end
